49bf43431e717004dd25ea1c63a7ac038bfa2539
joseacarmo17 <joseantoniocarmo17@gmail.com>
2020-01-15 16:42:59 +0100

    he empezado a modificar la rama prueba1

--- holamundoc++.cpp
+++ holamundoc++.cpp
@@ -5,7 +5,9 @@ using namespace std;
 int main() {
 
     cout << "Hola Mundo" << endl;
-
+    cout << "/* message */" << '\n';
+    cout << "Hola Mundo" << endl;
+    cout << "/* message */" << '\n';
     return 0;
 
 }
